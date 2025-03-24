8da0328ae568be5eaa38a65293c2431def10d48a
Marc Grupe <marc.grupe@gmail.com>
2025-03-24 15:57:45 -0400

    added css and put the customer order on the index.html file

--- Unit2.Froyo/src/app.js
+++ Unit2.Froyo/src/app.js
@@ -13,8 +13,8 @@
   }
 
 */
-
-const userInputString = prompt("Enter the flavors you want in a comma separated list", "Vanilla, Chocolate");
+let table = "";
+const userInputString = prompt("Enter the flavors you want in a comma separated list", "Vanilla,Vanilla,Vanilla,Chocolate,Strawberry,Coffee,Coffee");
 
 const flavorArr = userInputString.split(",");
 //console.log(flavorArr);
@@ -25,7 +25,7 @@ const getFlavorTotal = (flavors) => {
   for (const flavor of flavors) {
     flavor in totalFlavors ? totalFlavors[flavor]++ : totalFlavors[flavor] = 1;
   }
-  return totalFlavors
+  return totalFlavors;
 }
 
 const orderTotal = getFlavorTotal(flavorArr);
@@ -38,3 +38,14 @@ console.log("Customer Flavor Totals:");
 for (const flavor in orderTotal) {
  console.log(`${flavor}: ${orderTotal[flavor]}` );
 }
+
+table = '<tbody>';
+
+for(let key in orderTotal){
+  table += '<tr>';
+  table += '<td class="key">' + key + '</td>';
+  table += '<td>' + orderTotal[key] + '</td>';
+  table += '</tr>';
+}
+
+document.getElementById('tableData').innerHTML = table;
